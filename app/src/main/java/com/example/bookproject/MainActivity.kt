9f4bc68b02d6d5fa8bc27781b14cdb2803d0e698
JudithMallama <judith.mallama@udea.edu.co>
2022-02-15 22:15:20 -0500

    Prueba2 commmit
    Signed-off-by: JudithMallama <judith.mallama@udea.edu.co>

--- app/src/main/java/com/example/bookproject/MainActivity.kt
+++ app/src/main/java/com/example/bookproject/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-
+//Prueba2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
