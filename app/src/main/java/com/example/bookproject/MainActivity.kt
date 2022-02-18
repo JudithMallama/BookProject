915f189475f0b6b2fbe11513ebdaa9ceaa30ed6d
JudithMallama <judith.mallama@udea.edu.co>
2022-02-17 21:23:12 -0500

    Prueba4 commmit, ingreso, checkbooton
    Signed-off-by: JudithMallama <judith.mallama@udea.edu.co>

--- app/src/main/java/com/example/bookproject/MainActivity.kt
+++ app/src/main/java/com/example/bookproject/MainActivity.kt
@@ -2,15 +2,13 @@ package com.example.bookproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import com.example.bookproject.databinding.ActivityMainBinding
 
 //Prueba2
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainBinding :ActivityMainBinding
+    private lateinit var mainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +18,34 @@ class MainActivity : AppCompatActivity() {
 
         //val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
 
-        var numero : Int? = null
-        numero = null
-
         with(mainBinding) {
             saveButton.setOnClickListener {
 
-                if(nameBookEditText.text.isEmpty() ||
-                    nameAutorEditText.text.isNotEmpty() ||
-                    pagesEditText.text?.isEmpty() == true)
-                    {
-                        val nameBook: String = nameBookEditText.text.toString()
-                        val autorBook = nameAutorEditText.text.toString()
-                        val pagesBook = pagesEditText.text.toString().toInt()
-                        val resumeBook = resumeEditText.text.toString()
+                if (nameBookEditText.text.isEmpty() ||
+                    nameAutorEditText.text.isEmpty() ||
+                    pagesEditText.text?.isEmpty() == true
+                ) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Debe digitar nombre, autor y número de páginas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val nameBook: String = nameBookEditText.text.toString()
+                    val autorBook = nameAutorEditText.text.toString()
+                    val pagesBook = pagesEditText.text.toString().toInt()
+                    val resumeBook = resumeEditText.text.toString()
+                    var genereBook= ""
 
-                        infoTextView.text = "name: " + nameBook
-                        infoTextView.text = autorBook
-                        //mainBinding.infoTextView.text = pagesBook
-                        infoTextView.text = resumeBook//
-                }
+                    if (suspenceCheckBox.isChecked) genereBook =" Suspenso"
+                    if (terrorCheckBox.isChecked) genereBook +=" Terror"
+                    if(infantileCheckBox.isChecked) genereBook +=" Infantil"
+                    if(comicCheckBox.isChecked) genereBook +=" Comedia"
+
+
+                    infoTextView.text = getString(R.string.info, nameBook, autorBook, pagesBook, resumeBook, genereBook)
+              }
+
             }
         }
     }
