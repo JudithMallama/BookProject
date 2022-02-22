1ec91175a574a3373c435a11a0ab87b0393bd0d3
JudithMallama <judith.mallama@udea.edu.co>
2022-02-22 00:06:30 -0500

    Prueba commmit, interfaz de usuario

--- app/src/main/java/com/example/bookproject/MainActivity.kt
+++ app/src/main/java/com/example/bookproject/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.bookproject
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.bookproject.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 //Prueba2
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private var cal = Calendar.getInstance()
+    private var publicatioDate = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +22,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         //val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val format = "dd/MM/yyyy"
+            val simpleDateFormat = SimpleDateFormat(format, Locale.US)
+            publicatioDate = simpleDateFormat.format(cal.time).toString()
+            mainBinding.publicationDateButton.text = publicatioDate
+        }
 
         with(mainBinding) {
+
+            publicationDateButton.setOnClickListener {
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
             saveButton.setOnClickListener {
 
-                if (nameBookEditText.text.isEmpty() ||
-                    nameAutorEditText.text.isEmpty() ||
+                if (nameBookEditText.text?.isEmpty() == true ||
+                    nameAutorEditText.text?.isEmpty() == true ||
                     pagesEditText.text?.isEmpty() == true
                 ) {
                     Toast.makeText(
@@ -35,16 +61,34 @@ class MainActivity : AppCompatActivity() {
                     val autorBook = nameAutorEditText.text.toString()
                     val pagesBook = pagesEditText.text.toString().toInt()
                     val resumeBook = resumeEditText.text.toString()
-                    var genereBook= ""
+                    var genereBook = ""
 
-                    if (suspenceCheckBox.isChecked) genereBook =" Suspenso"
-                    if (terrorCheckBox.isChecked) genereBook +=" Terror"
-                    if(infantileCheckBox.isChecked) genereBook +=" Infantil"
-                    if(comicCheckBox.isChecked) genereBook +=" Comedia"
+                    if (suspenceCheckBox.isChecked) genereBook = " Suspenso"
+                    if (terrorCheckBox.isChecked) genereBook += " Terror"
+                    if (infantileCheckBox.isChecked) genereBook += " Infantil"
+                    if (comicCheckBox.isChecked) genereBook += " Comedia"
+
+                    val score = when {
+                        oneRadioButton.isChecked -> 1
+                        twoRadioButton.isChecked -> 2
+                        threeRadioButton.isChecked -> 3
+                        fourRadioButton.isChecked -> 4
+                        else -> 5
+                    }
 
 
-                    infoTextView.text = getString(R.string.info, nameBook, autorBook, pagesBook, resumeBook, genereBook)
-              }
+
+                    infoTextView.text = getString(
+                        R.string.info,
+                        nameBook,
+                        autorBook,
+                        pagesBook,
+                        resumeBook,
+                        genereBook,
+                        score,
+                        publicatioDate
+                    )
+                }
 
             }
         }
